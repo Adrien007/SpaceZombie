c9e8f6eeae8531558a326510caebfd3a6e8c543b
talatf1 <talaatf2005@gmail.com>
2026-03-19 14:03:41 -0400

    Ajout Background

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,18 +1,28 @@
 extends Control
 
+@onready var btn_language = $Control1/VBoxContainer/ButtonLanguage
+
+var is_english = true
+
+func _ready():
+    _update_language_button()
 
 func _on_start_button_pressed() -> void:
     get_tree().change_scene_to_file("res://Scenes/main_canva.tscn")
-    pass # Replace with function body.
-
 
 func _on_quit_pressed() -> void:
     get_tree().quit()
-    pass # Replace with function body.
 
+func _on_button_language_pressed() -> void:
+    is_english = !is_english
+    if is_english:
+        TranslationServer.set_locale("en")
+    else:
+        TranslationServer.set_locale("fr")
+    _update_language_button()
 
-func _on_button_english_pressed() -> void:
-    TranslationServer.set_locale("en")
-
-func _on_button_francais_pressed() -> void:
-    TranslationServer.set_locale("fr")
+func _update_language_button() -> void:
+    if is_english:
+        btn_language.text = "Français"
+    else:
+        btn_language.text = "English"
